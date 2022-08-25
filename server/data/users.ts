c2da956bca23fdf59e3a0204124295f3ee3c97c3
Özkan Parlakkılıç <79429368+ozkanparlakkilic@users.noreply.github.com>
2022-08-25 15:45:17 +0300

    add username mock data

--- server/data/users.ts
+++ server/data/users.ts
@@ -3,6 +3,7 @@ import bcryptjs from 'bcryptjs';
 const users = [
     {
         fullname: 'Özkan Parlakkılıç',
+        username: 'Ozkan54',
         email: 'prlkklc@gmail.com',
         password: bcryptjs.hashSync('1', 10),
         isAdmin: true,
@@ -10,12 +11,14 @@ const users = [
     },
     {
         fullname: 'Ömer Parlakkılıç',
+        username: 'Omer54',
         email: 'omerkral@gmail.com',
         password: bcryptjs.hashSync('123456789', 10),
         friends: [],
     },
     {
         fullname: 'Ekrem Parlakkılıç',
+        username: 'Ekrem54',
         email: 'ekoizm@gmail.com',
         password: bcryptjs.hashSync('159852', 10),
         friends: [],

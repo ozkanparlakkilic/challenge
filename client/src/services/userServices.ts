97fb28ad05aaafa1b3912ba4c15a48a07e749cb5
Özkan Parlakkılıç <79429368+ozkanparlakkilic@users.noreply.github.com>
2022-08-25 15:47:40 +0300

    create user services

---
+++ client/src/services/userServices.ts
@@ -0,0 +1,21 @@
+import axios from 'axios';
+import { baseUrl } from '../utils/getBaseUrl';
+
+const login = (username: string, password: string) =>
+    axios
+        .post(`${baseUrl}/api/users/login`, { username, password })
+        .then((res) => res.data)
+        .catch((err) => console.log(err));
+
+const register = (fullname: string, username: string, email: string, password: string) =>
+    axios
+        .post(`${baseUrl}/api/users/register`, {
+            fullname,
+            username,
+            email,
+            password,
+        })
+        .then((res) => res.data)
+        .catch((err) => console.log(err));
+
+export { login, register };

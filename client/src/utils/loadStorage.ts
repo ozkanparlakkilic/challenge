cce894ecccc47163b7a882e2521a814c6ba3c599
Özkan Parlakkılıç <79429368+ozkanparlakkilic@users.noreply.github.com>
2022-08-25 15:51:00 +0300

    add loadStorage get item function

---
+++ client/src/utils/loadStorage.ts
@@ -0,0 +1,7 @@
+function loadStorage<T>(item: string): T | null {
+    const localStorageItem = localStorage.getItem(item);
+    if (!localStorageItem) return null;
+    return JSON.parse(localStorageItem);
+}
+
+export { loadStorage };

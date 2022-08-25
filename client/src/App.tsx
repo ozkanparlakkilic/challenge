e4093753f6aa71c8931d18270867a4eb1b44a2f8
Özkan Parlakkılıç <79429368+ozkanparlakkilic@users.noreply.github.com>
2022-08-25 15:48:06 +0300

    add global app css and routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,38 @@
 import React from 'react';
 import './App.css';
+import Header from './components/common/Header/Header';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import LoginPage from './pages/LoginPage/LoginPage';
+import MainLayout from './layouts/MainLayout/MainLayout';
+import RegisterPage from './pages/RegisterPage/RegisterPage';
+import HomePage from './pages/HomePage/HomePage';
+import { loadStorage } from './utils/loadStorage';
+import { IUser } from './@types';
 
 function App() {
-  return (
-    <div className="App">
-      React
-    </div>
-  );
+    const user = loadStorage<IUser>('user');
+
+    console.log(user);
+    return (
+        <div className="App">
+            <Router>
+                <Header />
+                <MainLayout>
+                    {!user ? (
+                        <Routes>
+                            <Route path="/" element={<LoginPage />} />
+                            <Route path="login" element={<LoginPage />} />
+                            <Route path="register" element={<RegisterPage />} />
+                        </Routes>
+                    ) : (
+                        <Routes>
+                            <Route path="/" element={<HomePage />} />
+                        </Routes>
+                    )}
+                </MainLayout>
+            </Router>
+        </div>
+    );
 }
 
 export default App;

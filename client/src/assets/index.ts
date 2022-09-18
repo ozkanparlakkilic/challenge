be19f25b2f213058c1524daac0dfcd41c56606c6
Özkan Parlakkılıç <79429368+ozkanparlakkilic@users.noreply.github.com>
2022-09-18 19:01:51 +0300

    refactor assets folder

---
+++ client/src/assets/index.ts
@@ -0,0 +1,21 @@
+import QuizImage from '../assets/images/quiz.png';
+import AskUsImage from '../assets/images/ask-us.png';
+import ChallengeImage from '../assets/images/challenge.png';
+import QuestionImage from '../assets/images/question.png';
+import LeaderBoardImage from '../assets/images/leaderboard.png';
+import InformationImage from '../assets/images/information.png';
+import JokerImage from '../assets/images/joker.png';
+import EducationImage from '../assets/images/education.png';
+import FriendsImage from '../assets/images/friends.png';
+
+export {
+    QuizImage,
+    AskUsImage,
+    ChallengeImage,
+    QuestionImage,
+    LeaderBoardImage,
+    InformationImage,
+    JokerImage,
+    EducationImage,
+    FriendsImage,
+};

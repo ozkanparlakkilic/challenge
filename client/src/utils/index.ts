349b0155cab39732b90beb8929f72706d0e2f692
Özkan Parlakkılıç <79429368+ozkanparlakkilic@users.noreply.github.com>
2022-09-18 19:04:10 +0300

    refactor utils folder

---
+++ client/src/utils/index.ts
@@ -0,0 +1,5 @@
+import checkEmptyInputValue from './inputValueCheck/checkEmptyInputValue';
+import validateEmail from './inputValueCheck/validateEmail';
+import { baseUrl } from './baseUrl/getBaseUrl';
+
+export { checkEmptyInputValue, baseUrl, validateEmail };

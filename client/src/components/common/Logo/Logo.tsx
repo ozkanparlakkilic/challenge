9131fee971426268ac1e11fef5a6c6eda7128e54
Özkan Parlakkılıç <79429368+ozkanparlakkilic@users.noreply.github.com>
2022-09-20 19:31:46 +0300

    Add Logo component

---
+++ client/src/components/common/Logo/Logo.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Logo = () => {
+    return (
+        <div>
+            <img
+                width="250px"
+                height="170px"
+                src="https://upload.wikimedia.org/wikipedia/commons/d/db/Challenge_Logo.png"
+                alt="Challenge Me"
+                style={{ margin: "40px 0" }}
+            />
+        </div>
+    );
+};
+
+export default Logo;
--- client/src/components/common/index.ts
+++ client/src/components/common/index.ts
@@ -2,5 +2,7 @@ import Button from './Button/Button';
 import Input from './Input/Input';
 import ImageCard from './ImageCard/ImageCard';
 import Header from './Header/Header';
+import Logo from './Logo/Logo';
+import Form from './Form/Form';
 
-export { Button, Input, ImageCard, Header };
+export { Button, Input, ImageCard, Header, Logo, Form };

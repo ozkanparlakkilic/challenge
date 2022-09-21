90b0caab209380e57a68d63adb5f02ab1ed9177e
Özkan Parlakkılıç <79429368+ozkanparlakkilic@users.noreply.github.com>
2022-09-21 23:54:11 +0300

    add page title component

---
+++ client/src/components/common/PageTitle/PageTitle.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './PageTitle.css';
+
+interface PageTitleProps {
+    title: string;
+}
+
+const PageTitle = (props: PageTitleProps) => {
+    const { title } = props;
+
+    return <div className="page-title">{title}</div>;
+};
+
+export default PageTitle;

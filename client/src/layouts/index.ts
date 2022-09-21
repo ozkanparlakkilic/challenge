02bd6725ee81073bcec69ee5122406797951ab5c
Özkan Parlakkılıç <79429368+ozkanparlakkilic@users.noreply.github.com>
2022-09-21 23:56:17 +0300

    add scroll container

--- client/src/layouts/index.ts
+++ client/src/layouts/index.ts
@@ -1,5 +1,6 @@
 import ContentLayout from './ContentLayout/ContentLayout';
 import MainLayout from './MainLayout/MainLayout';
 import AuthLayout from './AuthLayout/AuthLayout';
+import ScrollContainer from './ScrollContainer/ScrollContainer';
 
-export { ContentLayout, MainLayout, AuthLayout };
+export { ContentLayout, MainLayout, AuthLayout, ScrollContainer };

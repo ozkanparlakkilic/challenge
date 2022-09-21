8845db6a8d77560362cf8fc110efff88a9f455f3
Özkan Parlakkılıç <79429368+ozkanparlakkilic@users.noreply.github.com>
2022-09-21 23:57:19 +0300

    add use navigate

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import {
     AskUsImage,
     ChallengeImage,
@@ -15,10 +16,13 @@ import { ContentLayout } from '../../layouts';
 import styles from './Home.module.css';
 
 const Home = () => {
+
+    const navigate = useNavigate();
+
     return (
         <ContentLayout classname={styles.extra_content_layout_style}>
             <div className={`d-flex ${styles.homepage_wrapper}`}>
-                <ImageCard source={QuizImage} title="Quiz" />
+                <ImageCard source={QuizImage} title="Quiz" onClick={() => navigate("quiz-list")} />
                 <ImageCard source={EducationImage} title="Education" />
                 <ImageCard source={JokerImage} title="Joker" />
                 <ImageCard source={QuestionImage} title="Question" />

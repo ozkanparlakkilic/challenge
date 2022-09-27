6c585f7210ff327796d99457fd93fe781abb083e
Özkan Parlakkılıç <79429368+ozkanparlakkilic@users.noreply.github.com>
2022-09-27 19:09:28 +0300

    add width and height prop

--- client/src/components/common/Logo/Logo.tsx
+++ client/src/components/common/Logo/Logo.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
 
 interface LogoProps {
-    classname?: string,
+    classname?: string | undefined,
     style?: object | undefined
+    width?: string | undefined,
+    height?: string | undefined
+
 }
 
-const Logo = ({classname,style}:LogoProps) => {
+const Logo = ({classname,style,width,height}:LogoProps) => {
     return (
         <div>
             <img
-                width="250px"
-                height="170px"
+                width={`${width ?? '250px'}`}
+                height={`${height ?? '170px'}`}
                 src="https://upload.wikimedia.org/wikipedia/commons/d/db/Challenge_Logo.png"
                 alt="Challenge Me"
                 style={style}

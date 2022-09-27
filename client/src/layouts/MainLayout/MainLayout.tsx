99c10c1c596bbd8f9359a5c621f96005f674340a
Özkan Parlakkılıç <79429368+ozkanparlakkilic@users.noreply.github.com>
2022-09-27 18:16:00 +0300

    set global user data with user data in localStorage

--- client/src/layouts/MainLayout/MainLayout.tsx
+++ client/src/layouts/MainLayout/MainLayout.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useAppDispatch } from '../../store/hooks';
+import { setUserData } from '../../store/slices/userSlice';
 import styles from './MainLayout.module.css';
 
 interface ChildrenProps {
@@ -6,6 +8,13 @@ interface ChildrenProps {
 }
 
 const MainLayout = ({ children }: ChildrenProps) => {
+
+    const dispatch = useAppDispatch()
+
+    useEffect(() => {
+        dispatch(setUserData())
+    },[dispatch])
+
     return <div className={`flex-1 ${styles.main_layout}`}>{children}</div>;
 };
 

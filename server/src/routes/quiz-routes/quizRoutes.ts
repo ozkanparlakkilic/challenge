765d677ee157b1692ee095ae2fb795dcdb595f3a
Özkan Parlakkılıç <79429368+ozkanparlakkilic@users.noreply.github.com>
2022-10-02 04:41:01 +0300

    add check auth middleware

--- server/src/routes/quiz-routes/quizRoutes.ts
+++ server/src/routes/quiz-routes/quizRoutes.ts
@@ -7,10 +7,11 @@ import {
     getQuizByIdWithQuestions,
     getQuizBySolverId,
 } from '../../controllers/quiz-controller/quizController';
+import { checkAuth } from '../../middlewares/auth/authMiddleware';
 
-router.route('/').get(getAllQuizzes);
-router.route('/quiz-detail/:id').get(getQuizById);
-router.route('/quiz/:id').get(getQuizByIdWithQuestions);
-router.route('/quiz-result/:id').get(getQuizBySolverId);
+router.route('/').get(checkAuth, getAllQuizzes);
+router.route('/quiz-detail/:id').get(checkAuth, getQuizById);
+router.route('/quiz/:id').get(checkAuth, getQuizByIdWithQuestions);
+router.route('/quiz-result/:id').get(checkAuth, getQuizBySolverId);
 
 export default router;

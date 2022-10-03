e8db6122ce3b3cc960ea89a62ff9a644c3f39cb9
Özkan Parlakkılıç <79429368+ozkanparlakkilic@users.noreply.github.com>
2022-10-03 23:56:47 +0300

    add lazy pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,28 +1,33 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
-import { Home, Information, Login, QuizDetail, Quizzes, Register } from './pages';
-import { Header } from './components/common';
+import { Login, QuizDetail, Quizzes, Register } from './pages';
+import { Header, Loading } from './components/common';
 import { MainLayout } from './layouts';
 import { PrivateRoute } from './routes/PrivateRouter';
+
+const Information = lazy(() => import('./pages').then(({ Information }) => ({ default: Information })));
+const Home = lazy(() => import('./pages').then(({ Home }) => ({ default: Home })));
 
 function App() {
     return (
         <div className="App">
             <Header />
             <MainLayout>
-                <Routes>
-                    <Route path="/" element={<PrivateRoute/>}>
-                        <Route path="/" element={<Home />} />
-                        <Route path="quiz-list" element={<Quizzes />} />
-                        <Route path="quiz-list/quiz-detail/:id" element={<QuizDetail />} />
-                        <Route path="information" element={<Information />} />
-                    </Route>
+                <Suspense fallback={ <Loading /> }>
+                    <Routes>
+                        <Route path="/" element={<PrivateRoute />}>
+                            <Route path="/" element={<Home />} />
+                            <Route path="quiz-list" element={<Quizzes />} />
+                            <Route path="quiz-list/quiz-detail/:id" element={<QuizDetail />} />
+                            <Route path="information" element={<Information />} />
+                        </Route>
 
-                    <Route path="login" element={<Login />} />
-                    <Route path="register" element={<Register />} />
-                </Routes>
-                </MainLayout>
+                        <Route path="login" element={<Login />} />
+                        <Route path="register" element={<Register />} />
+                    </Routes>
+                </Suspense>
+            </MainLayout>
         </div>
     );
 }

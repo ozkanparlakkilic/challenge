7b87b7ca1df1bff118e1eea6b881c4023f728d14
Özkan Parlakkılıç <79429368+ozkanparlakkilic@users.noreply.github.com>
2022-10-03 23:44:17 +0300

    add handling error callback function

--- server/src/middlewares/auth/authMiddleware.ts
+++ server/src/middlewares/auth/authMiddleware.ts
@@ -4,8 +4,12 @@ import jwt from 'jsonwebtoken';
 const checkAuth = (req: Request, res: Response, next: NextFunction) => {
     const token = req.headers.authorization?.split(' ')[1];
     if (token) {
-        jwt.verify(token, `${process.env.ACCESS_TOKEN_SECRET}`);
-        next();
+        jwt.verify(token, `${process.env.ACCESS_TOKEN_SECRET}`, (err) => {
+            if (err) {
+                return res.status(403).send({ message: 'Token is not valid' });
+            }
+            next();
+        });
     } else {
         return res.status(401).send({
             message: 'Auth failed',

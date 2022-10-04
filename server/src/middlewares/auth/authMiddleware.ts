7e9ab1dc1eb8f05ca7d39ada9615382fc11fbc38
Özkan Parlakkılıç <79429368+ozkanparlakkilic@users.noreply.github.com>
2022-10-04 19:42:23 +0300

    add jwt verify err type

--- server/src/middlewares/auth/authMiddleware.ts
+++ server/src/middlewares/auth/authMiddleware.ts
@@ -4,7 +4,7 @@ import jwt from 'jsonwebtoken';
 const checkAuth = (req: Request, res: Response, next: NextFunction) => {
     const token = req.headers.authorization?.split(' ')[1];
     if (token) {
-        jwt.verify(token, `${process.env.ACCESS_TOKEN_SECRET}`, (err) => {
+        jwt.verify(token, `${process.env.ACCESS_TOKEN_SECRET}`, (err: any) => {
             if (err) {
                 return res.status(403).send({ message: 'Token is not valid' });
             }

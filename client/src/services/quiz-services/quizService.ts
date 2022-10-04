40c0e8de606e9a79a06bc4769af6f6e6cb3fc434
Özkan Parlakkılıç <79429368+ozkanparlakkilic@users.noreply.github.com>
2022-10-04 21:41:21 +0300

    remove catch handling

--- client/src/services/quiz-services/quizService.ts
+++ client/src/services/quiz-services/quizService.ts
@@ -3,10 +3,7 @@ import { IQuiz } from '../../models';
 import { baseUrl } from '../../utils';
 
 const getAllQuiz = (userId: string | null): Promise<Array<IQuiz>> =>
-    axios
-        .get(`${baseUrl}/api/quizzes/`, { params: { id: userId } })
-        .then((res) => res.data)
-        .catch((err) => console.log(err));
+    axios.get(`${baseUrl}/api/quizzes/`, { params: { id: userId } }).then((res) => res.data);
 
 const getQuizDetail = (quizId: string | undefined): Promise<IQuiz> =>
     axios

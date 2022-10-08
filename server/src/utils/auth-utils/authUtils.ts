a885a80c0ce3a71226c0c51dbbae0f1e9079f6bd
Özkan Parlakkılıç <79429368+ozkanparlakkilic@users.noreply.github.com>
2022-10-08 23:40:48 +0300

    change expires tokens

--- server/src/utils/auth-utils/authUtils.ts
+++ server/src/utils/auth-utils/authUtils.ts
@@ -1,7 +1,6 @@
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import { IAccessToken, IRefreshToken } from '../../@types/token/tokenTypes';
-import { ACCESS_TOKEN_EXPIRE, REFRESH_TOKEN_EXPIRE } from '../../constants/contants';
 
 const hashPassword = async (password: string): Promise<string> => {
     const salt = await bcrypt.genSalt(10);
@@ -14,14 +13,14 @@ const comparePassword = (password: string, hashedPassword: string): Promise<bool
 
 const generateAccessToken = async (userId: string): Promise<IAccessToken> => {
     const accessToken = await jwt.sign({ userId }, `${process.env.ACCESS_TOKEN_SECRET}`, {
-        expiresIn: ACCESS_TOKEN_EXPIRE.toDateString(),
+        expiresIn: '1h',
     });
     return { accessToken };
 };
 
 const generateRefreshToken = async (userId: string): Promise<IRefreshToken> => {
     const refreshToken = await jwt.sign({ userId }, `${process.env.REFRESH_TOKEN_SECRET}`, {
-        expiresIn: REFRESH_TOKEN_EXPIRE.toDateString(),
+        expiresIn: '60d',
     });
     return { refreshToken };
 };

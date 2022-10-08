569bd23dd78de3caaf4314e7d77ebe1e4492763e
Özkan Parlakkılıç <79429368+ozkanparlakkilic@users.noreply.github.com>
2022-10-08 23:41:24 +0300

    fix get month name function bug

--- client/src/utils/date/getMonthName.ts
+++ client/src/utils/date/getMonthName.ts
@@ -1,7 +1,7 @@
 import EMonths from '@/models/Enumarables/Times/month';
 
 const getMonthName = (month: number): string | null => {
-    return EMonths[month];
+    return EMonths[month + 1];
 };
 
 export default getMonthName;
